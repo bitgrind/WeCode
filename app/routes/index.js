21d487c338fa2b45506ac9dfc6328676e4d02944
Keith Stedman <keithstedman@gmail.com>
2017-04-28 14:49:47 -0700

    added links

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,12 +4,18 @@ export default Ember.Route.extend({
   model() {
     return Ember.RSVP.hash({
       question: this.store.findAll('question')
+      link: this.store.findAll('link')
     });
   },
   actions: {
     saveQuestion3(params) {
       var newQuestion = this.store.createRecord('question', params);
       newQuestion.save();
+      this.transitionTo('index');
+    },
+    saveLink(params) {
+      var newLink = this.store.createRecord('link', params);
+      newLink.save();
       this.transitionTo('index');
     },
     saveResponse(params) {
